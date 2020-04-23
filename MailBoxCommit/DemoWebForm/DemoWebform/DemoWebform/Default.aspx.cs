d7c54c714576f4b7f27298c201684f8e1235577d
kaushik Patel <kaushikp160@gmail.com>
2020-04-23 17:28:08 +0530

    Second

---
+++ MailBoxCommit/DemoWebForm/DemoWebform/DemoWebform/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DemoWebform
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnLaunch_Click(object sender, EventArgs e)
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.Current.GetOwinContext().Authentication.Challenge(
+                  new AuthenticationProperties { RedirectUri = "/" },
+                  OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+    }
+}
---
+++ MailBoxCommit/MailIntegration/MailboxIntegration/Controllers/MailController.cs
@@ -0,0 +1,163 @@
+﻿using MailboxIntegration.Helpers;
+using MailboxIntegration.Models;
+using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MailboxIntegration.Controllers
+{
+    public class MailController : BaseController
+    {
+        public async Task<JsonResult> MailInboxSearch(string sharedMailId)
+        {
+            MailListDetail mailList = new MailListDetail();
+            var mailListDetail = (dynamic)null;
+            if (!string.IsNullOrEmpty(sharedMailId))
+            {
+                mailListDetail = await GraphHelper.OpenSharedMailbox(sharedMailId);
+            }
+            else
+            {
+                mailListDetail = await GraphHelper.GetEventsAsync();
+            }
+
+            mailList = GetMessageList(mailListDetail);
+
+            return Json(new
+            {
+                view = RenderRazorViewToString(ControllerContext, "MailboxView", mailList.Items.ToList()),
+                isValid = true,
+                itemsCount = mailList.Items.ToList()
+            }, JsonRequestBehavior.AllowGet);
+        }
+        public static string RenderRazorViewToString(ControllerContext controllerContext, string viewName, object model)
+        {
+            controllerContext.Controller.ViewData.Model = model;
+            using (var sw = new StringWriter())
+            {
+                var ViewResult = ViewEngines.Engines.FindPartialView(controllerContext, viewName);
+                var ViewContext = new ViewContext(controllerContext, ViewResult.View, controllerContext.Controller.ViewData, controllerContext.Controller.TempData, sw);
+                ViewResult.View.Render(ViewContext, sw);
+                ViewResult.ViewEngine.ReleaseView(controllerContext, ViewResult.View);
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            return View();
+        }
+
+        public MailListDetail GetMessageList(IEnumerable<Message> mailListDetail)
+        {
+            MailListDetail mailList = new MailListDetail();
+            List<AttachmentProperties> attachmentList;
+            List<MailCategories> mailCategoriesList;
+            List<MailListDetailItems> items = new List<MailListDetailItems>();
+            if (mailListDetail != null)
+            {
+                foreach (Message message in mailListDetail)
+                {
+                    attachmentList = new List<AttachmentProperties>();
+                    mailCategoriesList = new List<MailCategories>();
+                    if (message.Attachments != null)//message.Attachments.Count != 0 && 
+                    {
+                        foreach (var item in message.Attachments.CurrentPage.ToList())
+                        {
+                            attachmentList.Add(new AttachmentProperties
+                            {
+                                AttachmentName = item.Name,
+                                AttachmentSize = item.Size,
+                                AttachmentType = item.ODataType,
+                                AttachmentId = item.Id
+                            });
+                        }
+                    }
+                    if (message.Categories != null)
+                    {
+                        foreach (var item in message.Categories.ToList())
+                        {
+                            mailCategoriesList.Add(new MailCategories
+                            {
+                                CategoryName = item
+                            });
+                        }
+                    }
+
+
+                    items.Add(new MailListDetailItems
+                    {
+                        EmailID = message.From.EmailAddress.Address,
+                        Display = message.Subject,
+                        Id = message.Id,
+                        Message = message.BodyPreview,
+                        Properties = attachmentList,
+                        Categories = mailCategoriesList
+                    });
+                }
+                mailList.Items = items;
+            }
+            return mailList;
+        }
+
+        public async Task<object> DownLoadAttachment(MailListDetailItems mailListDetailItems)
+        {
+            try
+            {
+                var attachmentRequest = await GraphHelper.DownloadAttachments(mailListDetailItems.Id);
+
+                if (!System.IO.Directory.Exists(ConfigurationManager.AppSettings["filePath"]))
+                {
+                    CreateDiretoryIfNotExists(ConfigurationManager.AppSettings["filePath"]);
+                }
+                var filePath = ConfigurationManager.AppSettings["filePath"] + "/" + mailListDetailItems.EmailID;
+                if (!System.IO.Directory.Exists(filePath))
+                {
+                    CreateDiretoryIfNotExists(filePath);
+                }
+                foreach (var item in attachmentRequest)
+                {
+                    var fileAttachment = (FileAttachment)item;
+                    System.IO.File.WriteAllBytes(System.IO.Path.Combine(filePath, fileAttachment.Name), fileAttachment.ContentBytes);
+                }
+                var redirectUrl = new System.Web.Mvc.UrlHelper(Request.RequestContext).Action("Index", "Mail", new { });
+                return Json(new { Url = redirectUrl, status = "OK" });
+            }
+            catch (Exception ex)
+            {
+                Flash(ex.Message, null);
+                throw;
+            }
+
+        }
+
+        public string CreateDiretoryIfNotExists(string filePath)
+        {
+            System.IO.Directory.CreateDirectory(filePath);
+            return "";
+        }
+
+        public PartialViewResult OpenSharedMailbox(string sharedMailId)
+        {
+            try
+            {
+                MailListDetail mailList = new MailListDetail();
+                var mailListDetail = GraphHelper.OpenSharedMailbox(sharedMailId);
+                mailList = GetMessageList(mailListDetail.Result);
+                return PartialView("MailBoxView", mailList.Items.ToList());
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
